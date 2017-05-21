3fd85aeb39c9535ecd9062cd4413a013435b3dc0
Javier Panchi <javier.panchi@roames.com.au>
2017-05-21 14:50:13 +1000

    Service to Github

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,9 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import {GithubService} from '../services/github.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
+  providers:[GithubService]
 })
 export class HomeComponent implements OnInit {
 
---
+++ src/app/services/github.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import {Http} from '@angular/http';
+import 'rxjs/add/operator/map';
+
+@Injectable()
+export class GithubService {
+
+  constructor(private http:Http) { 
+
+  }
+
+  getRepositories(userName:String){
+    return this.http.get('https://api.github.com/users'+userName+'/repos')
+    .map(res=>res.json()).catch(this.showError);
+  }
+  private showError(error:Response){
+    alert("error:"+error.json());
+    return error.json();
+  }
+
+}

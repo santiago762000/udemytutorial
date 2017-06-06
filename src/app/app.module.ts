89c844cd43817797f600243cf49cc88f037411e3
Javier Panchi <javier.panchi@roames.com.au>
2017-06-06 22:02:54 +1000

    Reorganising and dialog

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,17 +7,23 @@ import {routing} from './app.routing';
 import { AppComponent } from './app.component';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import 'hammerjs';
-import {MdSnackBarModule,MdSelectModule, MdListModule, MdTabsModule, MdInputModule,MdCardModule,MdButtonModule,MdIconModule,MdToolbarModule} from '@angular/material';
+import {MdDialogModule,MdSnackBarModule,MdSelectModule, MdListModule, MdTabsModule, MdInputModule,MdCardModule,MdButtonModule,MdIconModule,MdToolbarModule} from '@angular/material';
 import { LoginComponent } from './login/login.component';
 import { HomeComponent } from './home/home.component';
 import { OrderByPipe } from './pipes/order-by.pipe';
+import { FavoriteTabComponent } from './home/favorite-tab/favorite-tab.component';
+import { SearchTabComponent } from './home/search-tab/search-tab.component';
+import { CommonComponent } from './dialogs/common/common.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
     HomeComponent,
-    OrderByPipe
+    OrderByPipe,
+    FavoriteTabComponent,
+    SearchTabComponent,
+    CommonComponent
   ],
   imports: [
     BrowserModule,
@@ -32,9 +38,13 @@ import { OrderByPipe } from './pipes/order-by.pipe';
     MdListModule, 
     MdTabsModule,
     MdSnackBarModule,
+    MdDialogModule,
     routing
   ],
   providers: [],
+  entryComponents: [
+    CommonComponent
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,55 +1,15 @@
 import { Component, OnInit } from '@angular/core';
-import {GithubService} from '../services/github.service';
-import {MdSnackBar} from '@angular/material';
+import { CommonComponent } from '.././dialogs/common/common.component';
 
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css'],
-  providers:[GithubService]
+  styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
 
-  username:String;
-  repoContents:Array<Object>=[];
-  showSearchBox:Boolean;
-  fieldOptions:Array<Object>;
-  selectedField:Object;
-  orderOptions:Array<Object>;
-  selectedOrder:Object;
-
-  constructor(private myService:GithubService, private snackBar:MdSnackBar) {
-   }
-
   ngOnInit() {
-    this.resetAll();
-    this.fieldOptions=[{name:"name",value:"name"},{name:"date",value:"pushed_at"}];
-    this.orderOptions=[{name:"Asc",value:1},{name:"Desc",value:-1}]
-    this.selectedField=this.fieldOptions[0];
-    this.selectedOrder=this.orderOptions[0];
-  }
-
-  showSnackBar(message:string, action:string){
-    this.snackBar.open(message,action,{duration:2000});
-  }
-
-  showContents(){
-    this.repoContents=[];
-    this.myService.getRepositories(this.username).subscribe(posts=>{
-      this.repoContents=posts;
-      this.showSearchBox=false;
-    }, err =>{
-      this.showSnackBar("Not Found", "Connection Error");
-      this.showSearchBox=true;
-    });
-
-  }
-
-  resetAll(){
-    this.showSearchBox=true;
-    this.username="";
-    this.repoContents=[];
   }
 
 }

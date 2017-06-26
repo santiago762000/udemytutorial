2c278bc6a6f13f617031314f816146ad3f4dae6f
Javier Panchi <javier.panchi@roames.com.au>
2017-06-26 21:52:49 +1000

    Configuration File

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,24 +1,32 @@
 import { Component, OnInit } from '@angular/core';
 import {Router} from '@angular/router';
 import {AccessGuardService} from './../services/access-guard.service';
+import {ConfigurationService} from './../services/configuration.service';
+import {MdSnackBar} from '@angular/material';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
+  providers: [ConfigurationService]
 })
 export class LoginComponent implements OnInit {
 
   email:String;
   password:String;
   router:Router;
+  properties:Object;
 
-  constructor(private _router:Router, private accessGuard:AccessGuardService) { 
+  constructor(private _router:Router, 
+  private accessGuard:AccessGuardService, 
+  private ConfigurationService:ConfigurationService,
+  private snackBar:MdSnackBar) { 
     this.router=_router;
   }
 
 
   ngOnInit() {
+    this.getConfigurations();
   }
 
   checkEmailPassword(){
@@ -30,6 +38,18 @@ export class LoginComponent implements OnInit {
       alert("Wrong email or password");
     }
 
+  }
+
+  private getConfigurations(){
+    this.ConfigurationService.getVariables().subscribe(posts=>{
+      this.properties=posts;
+    }, err=>{
+      this.showSnackBar("Connection Error","Please, check your configuration");
+    })
+  }
+
+  showSnackBar(message:string, action:string){
+    this.snackBar.open(message,action,{duration:2000});
   }
 
 }
